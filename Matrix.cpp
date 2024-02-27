ee20a791466f0c5cdb14b1b658d31245f12ca816
Liam <davisliam123@gmail.com>
2024-02-26 21:45:55 -0500

    start playing with matrices

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
 class Matrix {
+    public:
 
+    int rows;
+    int cols;
+
+    Matrix(int rows, int cols) {
+        rows = rows;
+        cols = cols;
+    }
 };
+
+int main() {
+    Matrix m(1, 2);
+}
